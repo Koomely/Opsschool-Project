c8d5bc16e7de71da4833258b0af092f417a8909c
koomely <koomely@gmail.com>
2019-06-24 16:26:07 +0300

    Finished bootstraping and configuring HAProxy with Consul as client

--- instances.tf
+++ instances.tf
@@ -24,7 +24,9 @@ resource "aws_key_pair" "generated_key" {
   public_key = "${tls_private_key.self-gen-key.public_key_openssh}"
 }
 
+#############################
 # HA-Proxy
+#############################
 
 resource "aws_instance" "ha-proxy" {
     count = "${var.count}"
@@ -33,46 +35,21 @@ resource "aws_instance" "ha-proxy" {
     instance_type = "t2.micro"
     vpc_security_group_ids = ["${aws_security_group.any.id}"]
     key_name = "${aws_key_pair.generated_key.key_name}"
-    
 
- #   iam_instance_profile   = "${aws_iam_instance_profile.consul-join.name}"
+    #   iam_instance_profile   = "${aws_iam_instance_profile.consul-join.name}"
 
-    user_data = "${file("instances/common/bootstrap.sh")}"
-    
-    provisioner "file" {
-        source      = "instances/ha-proxy/haproxy.cfg"
-        destination = "/home/ubuntu/haproxy.cfg"
-        connection {
-                user        = "ubuntu"
-                private_key = "${tls_private_key.self-gen-key.private_key_pem}"
-            }
-  }
-
-    provisioner "file" {
-        source      = "instances/ha-proxy/haproxy.service.json"
-        destination = "/home/ubuntu/haproxy.service.json"
-        connection {
-                user        = "ubuntu"
-                private_key = "${tls_private_key.self-gen-key.private_key_pem}"
-            }
-
-  }
-
-    provisioner "file" {
-        source      = "instances/ha-proxy/provision.yaml"
-        destination = "/home/ubuntu/provision.yaml"
-        connection {
-                user        = "ubuntu"
-                private_key = "${tls_private_key.self-gen-key.private_key_pem}"
-            }
-
-      
-  }
+ 
+  
   tags = {
     Name = "Final-Project-haproxy-${element(data.aws_availability_zones.az.names, count.index)}"
     consul-cluster = "consul-group"
   }
+user_data = "${file("instances/ha-proxy/bootstrap.sh")}"
 }
+
+
+
+
 
 output "HA-Proxy" {
   value = "${aws_instance.ha-proxy.*.public_ip}"
